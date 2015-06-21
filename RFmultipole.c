c867fb2bf10e919e635e2ef196c20d8fcba9bc03
BamyaRhaskar <ramyabhaskar834@gmail.com>
2015-06-21 13:46:19 -0700

    revised again

--- RFmultipole.c
+++ RFmultipole.c
@@ -1,97 +1,88 @@
 /*  Implementing RF multipole, version 1...progress so far...
-    9 warniings generated in total with gcc on Mac OSX
+    2 warniings generated in total with gcc on Mac OSX
+
+    Inputs given: n; R; KNn; L; vn; kRF; KSn; L; phin; x; y; psc
+    first match python program output
+    then manipulate current functions, with cos() and sin()
+    to match equations 159-161.
 */
+
 #include <stdio.h>
 #include <string.h>
 #include <math.h>
 #include "complex.h"
 
-// inputs given: n; R; KNn; L; vn; kRF; KSn; L; phin; x; y; psc
-    //first match python program output
-    //then manipulate current functions, with cos() and sin()
-    // to match equations 159-161.
 
-int factorial(int n)
-{ //couldn't find an already defined factorial function
+float factorial(int n)
+{ // couldn't find an already defined factorial function
   return (n == 1 || n == 0) ? 1 : factorial(n - 1) * n;
-} //end factorial
+} // end factorial
 
 
-double * ctaylor(double x, double y, double kn[], double ks[]){
-  int polySize = sizeof(kn)/sizeof(kn[0]);//generates warning,
-  double dpy = kn[polySize - 1];          //but its fine, really.
-  double dpx = ks[polySize - 1];          
+float * ctaylor(float x, float y, float kn[], float ks[], int polySize){
+  float dpx = kn[polySize - 1];          
+  float dpy = ks[polySize - 1];  
 
-  for (int i = 0; i < polySize; i++){
-
+  for (int i = (polySize-2); i >= 0 ; i--){ 
     dpx = kn[i] + (dpx*x - dpy*y)/((float)(i+1)); 
-    dpy = ks[i] + (dpx*x + dpy*y)/((float)(i+1)); 
-  
-  }
-  double dscrtXandY [2] = {dpx, dpy};
+    dpy = ks[i] + (dpx*y + dpy*x)/((float)(i+1)); 
 
-  printf("%d, %d", dscrtXandY[0], dscrtXandY[1]);
+  }
+  float realdXdY [2] = {dpx, dpy};
+  printf("%f, %f ", realdXdY[0], realdXdY[1]);
   printf("\n");
 
-  return dscrtXandY; //1 warning: fine for now...let memory be freed...
-} //end ctaylor
+  return realdXdY; // warning, OK: let memory be freed
+
+} // end ctaylor
 
 
-double * ztaylor(double x, double y, double kn[], double ks[]){
-  int polySize = sizeof(kn)/sizeof(kn[0]);//generates same warning
-  double complex z  = x + (1.0*I)*y;      //as line 16, but stil ok.
-  double complex res = 0;
+float * ztaylor(float x, float y, float kn[], float ks[], int polySize){
+  float complex z  = x + (1.0*I)*y; 
+  float complex res = 0;
 
   for (int i = 0; i < polySize; i++){
-
-    res += pow((kn[i] + (1.0*I)*ks[i])*z, (i+1))/factorial(i+1);
+    res += cpow((kn[i] + (1.0*I)*ks[i]) , i)/factorial(i);
 
   }
-  double complexReal [2] = {creal(res), cimag(res)};
-
-  printf("%d, %d", complexReal[0], complexReal[1]);
+  float complexReal [2] = {creal(res), cimag(res)};
+  printf("%f, %f ", complexReal[0], complexReal[1]);
   printf("\n");
 
-  return complexReal; //1 warning: fine for now...let memory be freed...
+  return complexReal; // warning, OK: let memory be freed
 
-} //end ztaylor
+} // end ztaylor
 
 
-double hornersMethod(double polynomial[], int n, int x)
-{
-  // what will be in here...
-}
  
 int main()
 {
-  double x = 2;
-  double y = 0;
-  double kn [5] = {1,3,3,4,1};
-  double kx [5] = {4,3,3,2,-4};
-  // double realTaylor [2];
-  // double complex complexTaylor [2];
+  float x = 2;
+  float y = 0;
+  float kn [5] = {1,3,3,4,1};
+  float kx [5] = {4,3,3,2,-4};
+  int polySize = 5;
 
-  ctaylor(x, y, kn, kx);
-  ztaylor(x, y, kn, kx);
+  ctaylor(x, y, kn, kx, polySize);
+  ztaylor(x, y, kn, kx, polySize);
 
-  for(int i = 1; i < 6; i++){
-    double knn [i];
-    double kxx [i];
+  for(int i = 1; i < (polySize+1); i++){
+    float knn [i];
+    float kxx [i];
 
     for (int j =0; j < i; j++){
       knn[j] = kn[j];
       kxx[j] = kx[j];
 
-    }
-    ctaylor(x, y, knn, kxx);
-    ztaylor(x, y, knn, kxx);
 
-    // printf("%d, %d", realTaylor[0], realTaylor[1]);
-    // printf("\n");
-    // printf("%d, %d", complexTaylor[0], complexTaylor[1]);
-    // printf("\n");
+    }    
+    ctaylor(x, y, kn, kx, polySize);
+    ztaylor(x, y, knn, kxx, i);
+
 
   }
+
+
 
   return 0;  
 
