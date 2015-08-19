eda60f81e30fa12a717e61c07715562594060e74
BamyaRhaskar <ramyabhaskar834@gmail.com>
2015-08-18 22:22:54 -0700

    implementation of zMomenta realized

--- exactBend.c
+++ exactBend.c
@@ -13,70 +13,82 @@
 		- Thick Dipole Equations
 */
 
+float zMomenta_0( float b1, float delta, float h, float px0, float py, float rho, float x){
 
-float alpha(float b1, float delta, float h, float theta, float px0, float py, float s, float x){
+	// float xMomenta_0 = px0; <- xMomenta at 0, is simple px0  
 
-	float alpha = arcsin( xMomenta(b1, delta, h, px0, x, s) / sqrt((1 + delta*delta) - (py*py) ) );
-
-	return alpha;
-
-} // end function alpha
-
-float zMomenta( float delta, float h, float L, float py, float s){
-
-	float theta = h*L;
-	float pz = sqrt( ((1 + delta)*(1 + delta)) - pow(xMomenta(b1, delta, h, px0, x, s), 2) - (py*py) );
+	float pz = sqrt( (1 + delta*delta) - pow(px0, 2) - (py*py) );
 
 	return pz;
 
 } // end function zMomenta
 
-float xMomenta (float b1, float delta, float h, float px0, float x, float s){
+float xMomenta (float b1, float delta, float h, float px0, float py, float rho, float s, float x){
 
 	float theta = h*s; 
+	float zMomenta_0  = sqrt( (1 + delta*delta) - pow(px0, 2) - (py*py) ); // <- xMomenta at 0, is simply px0  
 
-	float px = px0*cos(theta) + (zMomenta(delta, h, L, py, s) - b1 * (rho + x))*sin(theta);
+	float px = px0*cos(theta) + (zMomenta_0 - b1 * (rho + x))*sin(theta);
 
 	return px; 
 
 } // end function xMomenta
 
-float x (float b1, float delta, float h, float L,  float px0, float rho, float x){
 
-	float theta = h*L;
-	float x = (rho/b1)*((1/rho)*zMomenta(L, delta, theta) -  xmomentaPrime(px0, L, delta, b1, rho, x, theta) - b1); 
+float zMomenta( float b1, float delta, float h, float px0, float py, float rho, float s, float x){
 
-	return x; 
+	float theta = h*s;
+	float pz = sqrt( (1 + delta*delta) - pow(xMomenta(b1, delta, h, px0, py, rho, s, x), 2) - (py*py) );
 
-} // end function x
+	return pz;
 
-float xmomentaPrime( float b1, float delta, float h, float L, float px0, float rho, float x){
-
-	float theta = h*L; 
-	float pxPrime = -px0*sin(theta)*L  - b1 * (rho + x))*cos(theta)*L;
-
-	return pxPrime;
-
-} // end function xmomentaPrime
-
-float y ( float b1, float delta, float h, float L, float rho, float py, float y0){
-
-	float theta = h*L; 
-	float y = y0 + ((py*L)/(b1*rho)) + (py/L)*(alpha(0, delta, theta, py) - alpha(L, delta, theta, py));
-
-	return y;
-
-} // end function y
-
-float ct ( float b1, float ct0, float delta, float h, float L, float rho, float py){
-
-	float theta = h*L; 
-	float ct = ct0 + ((1 + delta)*L)/(b1*rho) + ((1+delta)/b1)*(alpha(0, delta, theta, py) - alpha(L, delta, theta, py));
-
-	return ct;
+} // end function zMomenta
 
 
-} // end function ct
+float alpha(float b1, float delta, float h, float px0, float py, float rho, float s, float x){
+
+	float alpha = asin( xMomenta(b1, delta, h, px0, py, rho, s, x) / sqrt((1 + delta*delta) - (py*py) ) );
+
+	return alpha;
+
+} // end function alpha
+
+// float x (float b1, float delta, float h, float L,  float px0, float rho, float x){
+
+// 	float theta = h*L;
+// 	float x = (rho/b1)*((1/rho)*zMomenta(L, delta, theta) -  xmomentaPrime(px0, L, delta, b1, rho, x, theta) - b1); 
+
+// 	return x; 
+
+// } // end function x
+
+// float xmomentaPrime( float b1, float delta, float h, float L, float px0, float rho, float x){
+
+// 	float theta = h*L; 
+// 	float pxPrime = -px0*sin(theta)*L  - b1 * (rho + x))*cos(theta)*L;
+
+// 	return pxPrime;
+
+// } // end function xmomentaPrime
+
+// float y ( float b1, float delta, float h, float L, float rho, float py, float y0){
+
+// 	float theta = h*L; 
+// 	float y = y0 + ((py*L)/(b1*rho)) + (py/L)*(alpha(0, delta, theta, py) - alpha(L, delta, theta, py));
+
+// 	return y;
+
+// } // end function y
+
+// float ct ( float b1, float ct0, float delta, float h, float L, float rho, float py){
+
+// 	float theta = h*L; 
+// 	float ct = ct0 + ((1 + delta)*L)/(b1*rho) + ((1+delta)/b1)*(alpha(0, delta, theta, py) - alpha(L, delta, theta, py));
+
+// 	return ct;
+
+
+// } // end function ct
 
 
 int main(){
